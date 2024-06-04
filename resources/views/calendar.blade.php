03eb0b05a8dfa2645b3303e8d3c9f428a8ef0fff
Jéssica Pedro <jessicapedro165@gmail.com>
2024-06-04 12:31:15 +0100

    update calendar

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -11,64 +11,82 @@
 @endpush
 
 @section('main')
-@if($nextRace)
 <div class="next-race">
-    <div class="info">
-        <div class="dat_country">
-            <div class="dat">
-                <p>{{ $nextRace->t1->format('d'). ' - ' .$nextRace->race->format('d') }}</p>
-                @if ($nextRace->t1->format('M') == $nextRace->race->format('M'))
-                <p class="month">{{ $nextRace->race->format('M') }}</p>
+        <a class="link" href="{{ route('calendar.show', ['id' => $nextRace->idCalendar]) }}">
+        <div class="info">
+            <div class="dat_country">
+                <div class="dat">
+                    <p>{{ \Carbon\Carbon::parse($nextRace->t1)->format('d') }} - {{\Carbon\Carbon::parse($nextRace->race)->format('d') }}</p>
+                    @if (\Carbon\Carbon::parse($nextRace->t1)->format('M') == \Carbon\Carbon::parse($nextRace->race)->format('M'))
+                    <p class="month">{{ \Carbon\Carbon::parse($nextRace->race)->format('M') }}</p>
+                    @else
+                    <p class="month">{{ \Carbon\Carbon::parse($nextRace->t1)->format('M')}} - {{\Carbon\Carbon::parse($nextRace->race)->format('M') }}</p>
+                    @endif
+                </div>
+                <img src="{{ url('storage/country/'.$nextRace->prix->imgCountry) }}" alt="{{ $nextRace->prix->country}}">
+            </div>
+            <hr>
+            <div class="country_gpname">
+                <p class="next_country">{{$nextRace->prix->country}}</p>
+                <p>{{$nextRace->prix->grandPrixName}}</p>
+            </div>
+            <hr>
+            <div class="parctice">
+                @if (!empty($nextRace->t1))
+                <p>Practice 1: {{\Carbon\Carbon::parse($nextRace->t1)->format('d/M')}}</p>
                 @else
-                <p class="month">{{ $nextRace->t1->format('M'). ' - ' .$nextRace->race->format('M') }}</p>
+
+                @endif
+
+                @if (!empty($nextRace->t2))
+                <p>Practice 2: {{\Carbon\Carbon::parse($nextRace->t2)->format('d/M')}}</p>
+                @else
+                @endif
+
+                @if (!empty($nextRace->t3))
+                <p>Practice 3: {{\Carbon\Carbon::parse($nextRace->t3)->format('d/M')}}</p>
+                @else
+                @endif
+
+                @if (!empty($nextRace->sprintQualify))
+                <p>Sprint Qualify: {{\Carbon\Carbon::parse($nextRace->sprintQualify)->format('d/M')}}</p>
+                @else
+                @endif
+
+                @if (!empty($nextRace->sprint))
+                <p>Sprint: {{\Carbon\Carbon::parse($nextRace->sprint)->format('d/M')}}</p>
+                @else
+                @endif
+
+                @if (!empty($nextRace->qualify))
+                <p>Qualify: {{\Carbon\Carbon::parse($nextRace->qualify)->format('d/M')}}</p>
+                @else
+                @endif
+
+                @if (!empty($nextRace->race))
+                <p>Race Day: {{\Carbon\Carbon::parse($nextRace->race)->format('d/M')}}</p>
+                @else
                 @endif
             </div>
-            <img src="{{ url('storage/country/'.$nextRace->prix->imgCountry) }}" alt="{{ $nextRace->prix->country}}">
         </div>
-        <hr>
-        <div class="country_gpname">
-            <p class="next_country">{{$nextRace->prix->country}}</p>
-            <p>{{$nextRace->prix->grandPrixName}}</p>
-        </div>
-        <hr>
-        <div class="parctice">
-            @if (!empty($nextRace->t1))
-            <p>Practice 1: {{$nextRace->t1->format('d M')}}</p>
-            @endif
-            @if (!empty($nextRace->t2))
-            <p>Practice 2: {{$nextRace->t2->format('d M')}}</p>
-            @endif
-            @if (!empty($nextRace->t3))
-            <p>Practice 3: {{$nextRace->t3->format('d/M')}}</p>
-            @endif
-            @if (!empty($nextRace->sprintQualify))
-            <p>Sprint Qualify: {{$nextRace->sprintQualify->format('d/M')}}</p>
-            @endif
-            @if (!empty($nextRace->sprint))
-            <p>Sprint: {{$nextRace->sprint->format('d/M')}}</p>
-            @endif
-            @if (!empty($nextRace->qualify))
-            <p>Qualify: {{$nextRace->qualify->format('d/M')}}</p>
-            @endif
-            @if (!empty($nextRace->race))
-            <p>Race Day: {{$nextRace->race->format('d/M')}}</p>
-            @endif
-        </div>
+    </a>
+        <img class="circuit" src="{{ url('storage/prix/'.$nextRace->prix->imgCircuts) }}" alt="">
     </div>
-    <img class="circuit" src="{{ url('storage/prix/'.$nextRace->prix->imgCircuts) }}" alt="">
-</div>
-@endif
+
+
+
 <h2 class="title">Next Races</h2>
 <div class="others">
-    @foreach ($calendar as $race)
+    @foreach ($upcomingRaces as $race)
+    <a class="link" href="{{ route('calendar.show', ['id' => $race->idCalendar]) }}">
         <div class="race">
             <div class="dat_country">
                 <div class="dat">
-                    <p>{{ $race->t1->format('d'). ' - ' .$race->race->format('d') }}</p>
-                    @if ($race->t1->format('M') == $race->race->format('M'))
-                    <p class="month">{{ $race->race->format('M') }}</p>
+                    <p>{{ \Carbon\Carbon::parse($race->t1)->format('d')}} - {{\Carbon\Carbon::parse($race->race)->format('d') }}</p>
+                    @if (\Carbon\Carbon::parse($race->t1)->format('M') == \Carbon\Carbon::parse($race->race)->format('M'))
+                    <p class="month">{{ \Carbon\Carbon::parse($race->race)->format('M') }}</p>
                     @else
-                    <p class="month">{{ $race->t1->format('M'). ' - ' .$race->race->format('M') }}</p>
+                    <p class="month">{{ \Carbon\Carbon::parse($race->t1)->format('M')}} - {{\Carbon\Carbon::parse($race->race)->format('M') }}</p>
                     @endif
                 </div>
                 <img src="{{ url('storage/country/'.$race->prix->imgCountry) }}" alt="{{ $race->prix->country}}">
@@ -80,9 +98,10 @@
             </div>
             <hr>
             <div class="image">
-               <img src="{{ url('storage/prix/'.$race->prix->imgCircuts) }}" alt="{{ $race->prix->name}}">
+                <img src="{{ url('storage/prix/'.$race->prix->imgCircuts) }}" alt="{{ $race->prix->name}}">
             </div>
         </div>
+    </a>
     @endforeach
 </div>
 @endsection
